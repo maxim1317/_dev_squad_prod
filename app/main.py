601cf67ec77ed7efee3b889889204c639117cda6
NastyaChirvonaya <nastyachirvonaya@gmail.com>
2020-03-30 00:07:41 +0300

    oauth try

--- app/main.py
+++ app/main.py
@@ -1,15 +1,78 @@
-from flask import Flask 
-from flask import render_template
-  
-app = Flask(__name__) 
-  
-@app.route("/") 
-@app.route("/index")
-def home_view():
-	user = {'username': 'User'}
-	return render_template('index.html', title='Home', user=user)
+from flask import Flask, redirect, url_for, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user,\
+    current_user
+from app.oauth import OAuthSignIn
 
-@app.route("/") 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['OAUTH_CREDENTIALS'] = {
+    'facebook': {
+        'id': '809126226276277',
+        'secret': '81f7f0e7a5f2bb64c51a0f94508dbb31'
+    }
+}
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.login_view = 'index'
+
+
 @app.route("/policy")
 def policy_view():
-	return render_template('policy.html', title='Privacy policy')
+    return render_template('policy.html', title='Privacy policy')
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/')
+def index():
+    user = {'username': 'User'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/auth')
+def oauth_authorize():
+    provider = 'facebook'
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+
+@app.route('/callback')
+def oauth_callback():
+    provider = 'facebook'
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
