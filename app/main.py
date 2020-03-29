acd96f297cf7fa9bba18ac2d3cf486eee2e0badb
NastyaChirvonaya <nastyachirvonaya@gmail.com>
2020-03-29 21:29:43 +0300

    html first try

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from flask import Flask 
+from flask import render_template
   
 app = Flask(__name__) 
   
 @app.route("/") 
-def home_view(): 
-        return "<h1>Welcome to this hell</h1>"
+@app.route("/index")
+def home_view():
+	user = {'username': 'User'}
+	return render_template('index.html', title='Home', user=user)
